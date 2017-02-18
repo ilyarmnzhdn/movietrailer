fb0d7ba3ea3cd23e466139965635b93cd7e9bd9d
Ilyar <im_vester@mail.ru>
2017-02-18 16:47:14 +0200

    Update media.py

--- media.py
+++ media.py
@@ -11,7 +11,8 @@ class Movie():
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     # Constructor
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image, 
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
